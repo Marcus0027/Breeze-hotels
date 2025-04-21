daf59527e0fa4f13cda221870384fb94fa094808
Marcus0027 <marcusroza27@gmail.com>
2025-04-21 17:14:43 -0300

    Update cadastrar.php

--- codes/pages/user/cadastrar.php
+++ codes/pages/user/cadastrar.php
@@ -26,14 +26,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="login.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
-    
-    <title>Cadastrar</title>
+    <link rel="icon" href="/breeze/images/logo.ico">
+    <title> Cadastrar </title>
 </head>
 <body class="d-flex align-items-center py-4">
 <main class="w-100 m-auto form-container" style="max-width: 900px; padding: 1rem;" >
