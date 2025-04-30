12e4a3a09702fac610e48aca10309d92ddc0fe11
Marcus André <marcusroza27@gmail.com>
2025-04-29 23:45:17 -0300

    Update conn.php

--- codes/conn/conn.php
+++ codes/conn/conn.php
@@ -1,15 +1,18 @@
 <?php
-    $serv = "localhost";
-    $user = "root";
-    $pass = "";
-    $bd = "breeze";
+$serv = "localhost";
+$user = "root";
+$pass = "";
+$bd = "breeze";
 
-    $conn = new mysqli($serv,$user,$pass,$bd);
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false
+];
 
-    try {
-        $conn = new PDO("mysql:host=$serv;dbname=$bd;charset=utf8", $user, $pass);
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    } catch(PDOException $e) {
-        die("Erro de conexão: " . $e->getMessage());
-    }
+try {
+    $conn = new PDO("mysql:host=$serv;dbname=$bd;charset=utf8mb4", $user, $pass, $options);
+} catch(PDOException $e) {
+    die("Erro de conexão: " . $e->getMessage());
+}
 ?>
